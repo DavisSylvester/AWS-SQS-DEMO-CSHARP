1ed8ab44c29c736a345e10963a6b7410b0a9c116
Davis Sylvester <davis@sylvesterllc.com>
2023-01-01 21:16:22 -0600

    WIP: SQS

--- src/HelloCdk/HelloCdkStack.cs
+++ src/HelloCdk/HelloCdkStack.cs
@@ -1,13 +1,34 @@
-using Amazon.CDK;
+using cdk = Amazon.CDK;
+using SQS = Amazon.CDK.AWS.SQS;
 using Constructs;
+using Amazon.CDK;
 
 namespace HelloCdk
 {
-    public class HelloCdkStack : Stack
+    public class HelloCdkStack : cdk.Stack
     {
-        internal HelloCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        internal HelloCdkStack(Construct scope, string id, cdk.IStackProps props = null) : base(scope, id, props)
         {
+            var DLtestQueue = new SQS.DeadLetterQueue() {
+                Queue = "DLQUEUE"
+            };
+
             // The code that defines your stack goes here
+            var testQueue = new SQS.Queue(this, "CdkLabQueue", new SQS.QueueProps
+            {
+                VisibilityTimeout = Duration.Seconds(300),
+                QueueName = "test-davis-one.fifo",
+                Fifo = true,
+                DeadLetterQueue = DLtestQueue
+            });
+
+            var testQueueTwo = new SQS.Queue(this, "CdkLabQueueTwo", new SQS.QueueProps
+            {
+                VisibilityTimeout = Duration.Seconds(300),
+                QueueName = "test-davis-two"
+            });
+
+
         }
     }
 }
